c2113c8a303f8d2568ff641cac332e039c32c329
Arsalan Naziri <arsalannaziri0786@gmail.com>
2024-07-13 06:16:38 +0000

    datatypes.js  file added

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "arsalan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gaya"
+
+// accountId= 123 // not allowed
+accountEmail = "arsalan0786@gmail.com"
+accountPassword = "1234"
+accountCity = "Patna"
+let accountState;
+
+console.log([accountId, accountEmail, accountCity, accountPassword]);
+
+console.table([accountId, accountEmail, accountCity, accountPassword, accountState])
+
+/*
+Prefer not to use var
+*/
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";  // traat code as newer js
+
+// alert(3 + 3) alert is not available in node js
+
+let name = 'Arsalan'
+let age = 27
+let isLoggedIn= false
+
+null //standalone value empty value
+undefined // when variable in created but value is yet not assigned
+
+console.table([typeof null, typeof undefined])
