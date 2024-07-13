5ef603a18e5755aa66a726747a90c15174a5cdd5
Arsalan Naziri <arsalannaziri0786@gmail.com>
2024-07-13 10:09:18 +0000

    datatype summary file add

---
+++ 01-basics/03_datatypeconversion.js
@@ -0,0 +1,33 @@
+// let score = undefined
+
+// console.log(typeof Number(score))
+// console.log(Number(score))
+
+// // let isLoggedIn = null
+// let booleanLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+// let num1= {
+//     name: "Arsalan",
+//     age: 27
+// }
+
+// let str1 = Number(num1)
+// console.log(typeof str1)
+// console.log(str1)
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(typeof("2" + 2))
+// console.log(typeof(2 + "2"))
+// console.log(typeof(2 + 2 + "2"))
+
+let x = 2
+let a = x++
+let b = ++x
+console.table([x, a, b])
+
+
---
+++ 01-basics/05_datatype-summary.js
@@ -0,0 +1,42 @@
+// Primary
+
+/* Types 
+String, Number, Boolean, null, undefined, symbol, Bigint
+*/
+
+// Reference type
+
+// Array, Objects, Function
+
+// let array1= [1, 2, 3]
+// function changeArray(arr) {
+//     arr[2]= 5
+//     arr + [7]
+//     return arr
+
+// }
+// console.log(array1)
+// changeArray(array1)
+// console.log(array1)
+
+// let id = Symbol(123)
+// let anotherId = Symbol(123)
+
+// console.table(id == anotherId)
+
+// let bigNumber = 1n
+// console.log(typeof id)
+
+// let myObj = { name: "Arsalan", age: 27}
+
+
+// const myFunc = function() {
+//     console.log("Hello")
+// }
+
+// console.log(typeof array1)
+
+// stack Heap
+
+let myName = ""
+
